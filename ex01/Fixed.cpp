c8799818e3dc06c460ba05c528a994100faccae9
Yazid Bokhari <yabokhar@z1r2p2.42lyon.fr>
2025-09-22 16:23:23 +0200

    little changes in ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: yabokhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 16:19:34 by yabokhar          #+#    #+#             */
-/*   Updated: 2025/09/11 13:59:35 by yabokhar         ###   ########.fr       */
+/*   Updated: 2025/09/22 16:20:05 by yabokhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,28 @@ Fixed::Fixed(void)
 
 {
 	std::cout << "Default constructor called" << std::endl;
-	this->_value = 0;
+	_value = 0;
 }
 
 Fixed::Fixed(int const value)
 
 {
 	std::cout << "Int constructor called" << std::endl;
-	this->_value = value << _fractional_bits_number;
+	_value = value << _fractional_bits_number;
 }
 
 Fixed::Fixed(float const value)
 
 {
 	std::cout << "Float constructor called" << std::endl;
-	this->_value = roundf(value * (1 << _fractional_bits_number));
+	_value = roundf(value * (1 << _fractional_bits_number));
 }
 
 Fixed::Fixed(const Fixed &old)
 
 {
 	std::cout << "Copy constructor called" << std::endl;
-	this->_value = old._value;
+	_value = old._value;
 }
 
 Fixed&	Fixed::operator=(Fixed const &old)
@@ -47,16 +47,20 @@ Fixed&	Fixed::operator=(Fixed const &old)
 	std::cout << "Copy assignment operator called" << std::endl;
 
 	if (this != &old)
-		this->_value = old._value;
-	return *this;
+		_value = old._value;
+	return (*this);
 }
 
-Fixed::~Fixed(void) { std::cout << "Destructor called" << std::endl ;}
+Fixed::~Fixed(void) 
+
+{
+	std::cout << "Destructor called" << std::endl ;
+}
 
 int	Fixed::getRawBits(void) const
 
 {
-	return (this->_value);
+	return (_value);
 }
 
 void	Fixed::setRawBits(int const raw)
@@ -68,13 +72,13 @@ void	Fixed::setRawBits(int const raw)
 float	Fixed::toFloat(void) const
 
 {
-	return ((float)this->_value / (1 << _fractional_bits_number));
+	return ((float)_value / (1 << _fractional_bits_number));
 }
 
 int Fixed::toInt(void) const
 
 {
-	return (this->_value >> _fractional_bits_number);
+	return (_value >> _fractional_bits_number);
 }
 
 std::ostream&	operator<<(std::ostream &out, Fixed const &n)
